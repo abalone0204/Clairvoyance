2e5651344fdf82481ffe91d397c3bab47603d26d
abalone0204 <abalone0204@gmail.com>
2016-06-05 11:52:01 +0800

    refactor(auth): split to smaller functions

--- front-end/chrome/oauthCallback.js
+++ front-end/chrome/oauthCallback.js
@@ -7,7 +7,10 @@ const {
     host
 } = config.dev.auth
 
-const scope = ['email', 'public_profile', 'user_about_me', 'user_posts']
+const scope = [
+    'email', 'public_profile', 'user_about_me', 
+    'user_posts', 'user_tagged_places'
+]
 const url = `https://www.facebook.com/dialog/oauth?client_id=${clientId}&redirect_uri=${redirectUri}&auth_type=rerequest&scope=${scope.join(',')}`
 
 function getQueryString(url, field) {
@@ -16,42 +19,52 @@ function getQueryString(url, field) {
     return string ? string[1] : null;
 }
 
+function doneFetchCode(authTabId, tabId, changeInfo, tab) {
+    return (
+        tabId == authTabId &&
+        changeInfo.status == 'complete' &&
+        tab.url.startsWith(redirectUri)
+    )
+}
+
+
+function tabUpdateHandler(authTabId, originTabIndex, sendResponse) {
+    return (tabId, changeInfo, tab) => {
+        if (doneFetchCode(authTabId, tabId, changeInfo, tab)) {
+            const code = getQueryString(tab.url, 'code')
+            const options = {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                mode: 'cors',
+                body: JSON.stringify({
+                    code
+                })
+            }
+
+            fetch(authUri, options)
+                .then(response => response.json())
+                .then(data => {
+                    sendResponse({data})
+                    chrome.tabs.highlight({
+                        'tabs': originTabIndex
+                    }, () => chrome.tabs.remove(tabId))
+
+                })
+
+        }
+    }
+}
+
 export default function callback(originTabIndex, sendResponse) {
     let authTabId
 
     chrome.tabs.create({
-            url: url,
-            selected: true
-        }, (tab) => {
-            authTabId = tab.id;
-            chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-                    if (
-                        tabId == authTabId &&
-                        changeInfo.status == 'complete' &&
-                        tab.url.startsWith(redirectUri)
-                    ) {
-                        const code = getQueryString(tab.url, 'code')
-                        const options = {
-                            method: 'POST',
-                            headers: {
-                                "Content-Type": "application/json"
-                            },
-                            mode: 'cors',
-                            body: JSON.stringify({code})
-
-                        }
-                        fetch(authUri, options)
-                            .then(response => response.json())
-                            .then(data => {
-                                console.log('data===>', data);
-                                sendResponse({data})
-                                chrome.tabs.highlight({
-                                    'tabs': originTabIndex
-                                }, () => chrome.tabs.remove(tabId))
-                            })
-
-                    }
-            })
-
+        url: url,
+        selected: true
+    }, (tab) => {
+        authTabId = tab.id;
+        chrome.tabs.onUpdated.addListener(tabUpdateHandler(authTabId, originTabIndex, sendResponse))
     })
 }
