5de84478d18a85ba2d745b0d645331c7e9d5eeb7
abalone0204 <abalone0204@gmail.com>
2016-06-07 12:04:10 +0800

    fix: watch fetch job saga, and add job reducer

--- front-end/app/sagas/index.js
+++ front-end/app/sagas/index.js
@@ -1,10 +1,12 @@
 import {watchRequestLogin} from './login.js'
 import {watchRequestLeaveComment} from './leaveComment.js'
 import {watchRequestFetchComments} from './fetchComments.js'
+import {watchRequestFetchJob} from './fetchJob.js'
 export default function* rootSaga() {
     yield [
         watchRequestLogin(),
         watchRequestLeaveComment(),
-        watchRequestFetchComments()
+        watchRequestFetchComments(),
+        watchRequestFetchJob()
     ]
 }
