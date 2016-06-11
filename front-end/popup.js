a472374ba6ac9651441de8e678f0abb06949905b
abalone0204 <abalone0204@gmail.com>
2016-06-12 03:21:10 +0800

    feat(chrome): popup

--- front-end/popup.js
+++ front-end/popup.js
@@ -1,9 +1,22 @@
+
 class Popup extends React.Component {
     render() {
         return (
-            <div>
-                <h2>Popup</h2>
-                <a href="" onClick={()=>chrome.tabs.create({'url': "/options.html" } )}>選項</a>
+            <div style={{width: '360px', fontSize: '18px', padding: '15px'}}>
+                <h3>Clairvoyance</h3>
+                <div>
+                    <a href="" onClick={()=>chrome.tabs.create({'url': "/options.html" } )}><h4>設置與說明書</h4></a>
+                </div>
+                <div>
+                    <h4>關於 Clairvoyance</h4>
+                    <div>
+                        臉書專頁：<a href="https://www.facebook.com/Clairvoyance-%E6%B1%82%E8%81%B7%E5%A4%A9%E7%9C%BC%E9%80%9A-1084564708284768" target="_blank">Clairvoyance - 求職天眼通</a>
+                    </div>
+                    <div>
+                        Source code: <a href="https://github.com/abalone0204/Clairvoyance" target="_blank">Clairvoyance</a>
+                    </div>
+                </div>
+                
             </div>
         )
     }
