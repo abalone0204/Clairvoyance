90c87e238b836890a9238af8325daddb18e16eef
abalone0204 <abalone0204@gmail.com>
2016-07-15 23:36:41 +0800

    fix: Fix provider of 1111. And the flash while there is no warning in job helper

--- front-end/app/components/JobHelper/index.js
+++ front-end/app/components/JobHelper/index.js
@@ -3,81 +3,81 @@ import styles from './styles.css'
 import Modal from 'react-modal'
 
 import {
-    getCompanyInfo,
-    searchPackageByNameApi,
-    checkName,
-    getPackageInfo
+  getCompanyInfo,
+  searchPackageByNameApi,
+  checkName,
+  getPackageInfo
 } from 'jobHelper'
 
 const customStyle = {
-    overlay: {
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        backgroundColor: 'rgba(0, 0, 0, 0.6)',
-        zIndex: 2000
-    },
-    content: {
-        position: 'absolute',
-        top: '40px',
-        left: '40px',
-        right: '40px',
-        bottom: '40px',
-        border: '1px solid #ccc',
-        background: '#fff',
-        overflow: 'auto',
-        WebkitOverflowScrolling: 'touch',
-        borderRadius: '4px',
-        outline: 'none',
-        padding: '20px',
-        zIndex: 2001
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    zIndex: 2000
+  },
+  content: {
+    position: 'absolute',
+    top: '40px',
+    left: '40px',
+    right: '40px',
+    bottom: '40px',
+    border: '1px solid #ccc',
+    background: '#fff',
+    overflow: 'auto',
+    WebkitOverflowScrolling: 'touch',
+    borderRadius: '4px',
+    outline: 'none',
+    padding: '20px',
+    zIndex: 2001
 
-    }
+  }
 }
 
 class JobHelper extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            result: [],
-            display: false,
-            displayFlash: false
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      result: [],
+      display: false,
+      displayFlash: false
     }
-    componentDidMount() {
-        const params = getCompanyInfo()
-        if ('object' == typeof(params) && 'undefined' !== typeof(params.name)) {
-            searchPackageByNameApi(params.name, params.link, (result) => {
-                this.setState({
-                    result,
-                    displayFlash: true
-                })
-            }, (error_message) => {
-                searchPackageByNameApi(params.name, function(result) {
-                    console.log("result while name api doesn't work:", result);
-                }, checkName);
-            })
-        }
+  }
+  componentDidMount() {
+    const params = getCompanyInfo()
+    if ('object' == typeof(params) && 'undefined' !== typeof(params.name)) {
+      searchPackageByNameApi(params.name, params.link, (result) => {
+        this.setState({
+          result,
+          displayFlash: true
+        })
+      }, (error_message) => {
+        searchPackageByNameApi(params.name, function(result) {
+          console.log("result while name api doesn't work:", result);
+        }, checkName);
+      })
     }
-    render() {
-        const {
-            result,
-            display,
-            displayFlash
-        } = this.state
+  }
+  render() {
+    const {
+      result,
+      display,
+      displayFlash
+    } = this.state
 
-        if (result.length > 0) {
-            const resultNode = result.map((item, key) => {
-                const {
-                    date,
-                    name,
-                    reason,
-                    link
-                } = item
-                return (
-                    <div key={item.link+key} styleName='item'>
+
+    const resultNode = result.map((item, key) => {
+      const {
+        date,
+        name,
+        reason,
+        link
+      } = item
+      return (
+        <div key={item.link+key} styleName='item'>
                         <div>
                             時間： {date}
                         </div>
@@ -91,26 +91,26 @@ class JobHelper extends React.Component {
                            <a href={link} target='_blank'>官方公告連結</a>   
                         </div>
                     </div>
-                )
-            })
-            const handler = (e) => {
-                this.setState({
-                    display: true
-                })
-            }
-            const closeHandler = (_) => {
-                this.setState({
-                    display: false
-                })   
-            }
+      )
+    })
+    const handler = (e) => {
+      this.setState({
+        display: true
+      })
+    }
+    const closeHandler = (_) => {
+      this.setState({
+        display: false
+      })
+    }
 
-            const closeFlashHandler = (_) => {
-                this.setState({
-                    displayFlash: false
-                })
-            }
-            return (
-                <div styleName='container'>
+    const closeFlashHandler = (_) => {
+      this.setState({
+        displayFlash: false
+      })
+    }
+    return (
+      <div styleName='container'>
                     <Modal
                       isOpen={display}
                       // onAfterOpen={afterOpenFn}
@@ -128,29 +128,30 @@ class JobHelper extends React.Component {
                     </Modal>
                     {
                         displayFlash ?
-                            <div styleName='notice'>
-                                <div styleName='flash-container'>
-                                    <i onClick={closeFlashHandler} styleName='close-flash' className="fa fa-times" aria-hidden="true" />
-                                    有 {result.length} 筆違反勞基法紀錄，
-                                    <div styleName='show' onClick={handler}>點這裡看全部</div>
-                                </div>
-                            </div>
+                            result.length > 0 ?
+                              <div styleName='notice'>
+                                  <div styleName='flash-container'>
+                                      <i onClick={closeFlashHandler} styleName='close-flash' className="fa fa-times" aria-hidden="true" />
+                                      有 {result.length} 筆違反勞基法紀錄，
+                                      <div styleName='show' onClick={handler}>點這裡看全部</div>
+                                  </div>
+                              </div>
+                              :
+                              <div styleName='notice'>
+                                  <div styleName='flash-container--alright'>
+                                      <i onClick={closeFlashHandler} styleName='close-flash' className="fa fa-times" aria-hidden="true" />
+                                      目前查無違反勞基法紀錄 ：）
+                                  </div>
+                              </div>
                             :
                             null
                     }
                     
                 </div>
-            )
-        } else {
-            return (
-                <div>
-                    目前查無違反勞基法紀錄
-                </div>
-            )
+    )
 
-        }
 
-    }
+  }
 }
 
 export default CSSModules(JobHelper, styles)
--- front-end/app/providers/1111.js
+++ front-end/app/providers/1111.js
@@ -1,6 +1,14 @@
-const get_job_name = () => document.querySelector('#commonTop h1').innerText.trim()
+const get_job_name = () => (document.querySelector('#commonTop h1').innerText.trim())
 const get_company_name = () => document.querySelector('#commonTop .navbar a').innerText.trim()
-const get_eeee_job_no = () => location.search.match(/\?eNo=([0-9]+)/)[1] 
+const get_eeee_job_no = () =>{
+  if (location.search.match(/\?eNo=([0-9]+)/) !== null) {
+    return location.search.match(/\?eNo=([0-9]+)/)[1]
+  }
+  if (location.hash.match(/\?eNo=([0-9]+)/) !== null) {
+    return location.hash.match(/\?eNo=([0-9]+)/)[1]
+  }
+  return null
+}
 
 const provider = {
     get_job_name,
