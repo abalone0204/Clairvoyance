691000ae4910e67c4e44a7cdbe5e869da1954103
abalone0204 <abalone0204@gmail.com>
2016-09-11 16:36:41 +0800

    feat(API): Add goodjob/searchByJob API

---
+++ front-end/app/API/goodjob/searchByJob.js
@@ -0,0 +1,27 @@
+import {
+  checkStatus,
+  parseJSON,
+} from './helper.js'
+
+import config from '../../../config.json'
+
+export default function searchByJob({
+  job_title,
+  page,
+}) {
+  const {
+    backend
+  } = config
+  const options = {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: 'GET',
+    mode: 'cors'
+  }
+  const baseURL = `https://tranquil-fortress-92731.herokuapp.com/clairvoyance/search/by-job?job_title=${job_title}`
+  const url = page ? `${baseURL}&page=${page}` : `${baseURL}`
+  return fetch(url, options)
+    .then(checkStatus)
+    .then(parseJSON)
+}
--- front-end/app/API/helper.js
+++ front-end/app/API/helper.js
@@ -1,13 +1,16 @@
 export function checkStatus(response) {
-    if (response.status >= 200 && response.status < 300) {
-        return response
-    } else {
-        var error = new Error(response.statusText)
-        error.response = response
-        throw error
-    }
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
 }
 
 export function parseJSON(response) {
-    return response.json()
+  return response.text()
+    .then((text) => {
+      return text ? JSON.parse(text) : {}
+    })
 }
