21d7a62d457e3bbae29b7e2ae5dd45d8d6350581
abalone0204 <abalone0204@gmail.com>
2016-09-11 14:01:48 +0800

    add ga

--- front-end/eventPage.js
+++ front-end/eventPage.js
@@ -2,16 +2,29 @@ import config from '../config.json'
 import oauthCallback from './chrome/oauthCallback.js'
 
 const {
-    authUri
+  authUri
 } = config.dev.auth
 
+  (function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})()
+
+const _gaq = _gaq || []
+_gaq.push(['_setAccount', 'UA-82256880-1'])
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        console.log('request===>',request);
-        if (request.message === 'login') {
-            oauthCallback(sender.tab.index, sendResponse)       
-            return true
-        }
+  function(request, sender, sendResponse) {
+    if (request.message === 'login') {
+      oauthCallback(sender.tab.index, sendResponse)
+      return true
     }
+    if(request.message === 'clickEvent') {
+      
+    }
+  }
 )
