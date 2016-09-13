87065bfc5aff43d49e85202b2b9cc269859f9f4f
abalone0204 <abalone0204@gmail.com>
2016-09-13 12:01:54 +0800

    add init to root reducer

--- front-end/app/reducers/index.js
+++ front-end/app/reducers/index.js
@@ -5,8 +5,9 @@ import comments from './comments.js'
 import user from './user.js'
 import job from './job.js'
 import workingTimes from './workingTimes.js'
-
+import init from './init.js'
 const rootReducer = combineReducers({
+  init,
   comments,
   user,
   job,
