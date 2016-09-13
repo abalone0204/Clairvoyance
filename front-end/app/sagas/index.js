e05d65c7c13a7c57df3fa9e8ccc931f53bc24a45
abalone0204 <abalone0204@gmail.com>
2016-09-13 12:01:14 +0800

    feat(): Init job object

--- front-end/app/sagas/index.js
+++ front-end/app/sagas/index.js
@@ -1,19 +1,38 @@
-import {watchRequestLogin} from './login.js'
-import {watchRequestLeaveComment} from './leaveComment.js'
-import {watchRequestFetchComments} from './fetchComments.js'
-import {watchRequestFetchJob} from './fetchJob.js'
-import {watchRequestCreateJob} from './createJob.js'
 import {
-    transactionOfJobAndComments
+  watchRequestLogin
+} from './login.js'
+import {
+  watchRequestLeaveComment
+} from './leaveComment.js'
+import {
+  watchRequestFetchComments
+} from './fetchComments.js'
+import {
+  watchRequestFetchJob
+} from './fetchJob.js'
+import {
+  watchRequestCreateJob
+} from './createJob.js'
+import {
+  transactionOfJobAndComments
 } from './transactionFlows.js'
+import {
+  watchRequestFetchWorkingTimes,
+} from './fetchWorkingTimes.js'
+
+import {
+  watchFetchInitJobObject,
+} from './fetchInitJobObject.js'
 
 export default function* rootSaga() {
-    yield [
-        watchRequestLogin(),
-        watchRequestLeaveComment(),
-        watchRequestFetchComments(),
-        watchRequestFetchJob(),
-        watchRequestCreateJob(),
-        transactionOfJobAndComments()
-    ]
+  yield [
+    watchRequestLogin(),
+    watchRequestLeaveComment(),
+    watchRequestFetchComments(),
+    watchRequestFetchJob(),
+    watchRequestCreateJob(),
+    transactionOfJobAndComments(),
+    watchRequestFetchWorkingTimes(),
+    watchFetchInitJobObject(),
+  ]
 }
