119f514de062d8fba782ef47bebdac9aba88c911
abalone0204 <abalone0204@gmail.com>
2016-09-13 11:00:04 +0800

    test(): init job object

---
+++ tests/front-end/reducers/init.js
@@ -0,0 +1,60 @@
+import {
+  assert
+} from 'chai'
+import {
+  SET_INIT_JOB_OBJECT,
+  FETCH_INIT_JOB_OBJECT,
+  FAIL_TO_INIT_JOB_OBJECT,
+  fetchInitJobObject,
+  setInitJobObject,
+  failToInitJobObject,
+} from 'actions/initialize.js'
+import {
+  jobObject,
+} from 'reducers/init.js'
+describe('Reducer/ init', function() {
+  describe('jobObject', function() {
+    const initState = {
+      status: 'init',
+    }
+    it('should handle init of initJobObject', () => {
+      const expected = {
+        status: 'init',
+      }
+      const actual = jobObject(initState, {})
+      assert.deepEqual(expected, actual)
+    })
+
+    it('should handle fetch initJobObject', () => {
+      const expected = {
+        status: 'fetching',
+      }
+      const actual = jobObject(initState, fetchInitJobObject())
+      assert.deepEqual(expected, actual)
+    })
+
+    it('should handle set initJobObject', () => {
+      const mock = {
+        job_name: 'engineer',
+        company_name: 'qooqle',
+      }
+      const expected = {
+        status: 'completed',
+        ...mock
+      }
+      const actual = jobObject(initState, setInitJobObject(mock))
+      assert.deepEqual(expected, actual)
+    })
+
+    it('should handle fail to fetch initJobObject', () => {
+      const error = new Error('fail to fetch initJobObject')
+      const expected = {
+        status: 'error',
+        error,
+      }
+      const actual = jobObject(initState, failToInitJobObject(error))
+      assert.deepEqual(expected, actual)
+    })
+
+  });
+});
